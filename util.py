4bbbf9a65c7d9959f3963082090aa2404e783d37
Francis Tseng <f+accounts@frnsys.com>
2021-05-30 20:49:43 -0400

    show error message

--- util.py
+++ util.py
@@ -9,7 +9,9 @@ def download(url, outfile):
     """download a file"""
     fname = url.split('/')[-1]
     with requests.get(url, stream=True) as r:
-        r.raise_for_status()
+        if r.status_code != 200:
+            print(r.json())
+            r.raise_for_status()
         with open(outfile, 'wb') as f:
             for chunk in r.iter_content(chunk_size=8192):
                 if chunk: # filter out keep-alive new chunks
